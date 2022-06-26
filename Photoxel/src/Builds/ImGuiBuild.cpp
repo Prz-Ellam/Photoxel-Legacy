8bb3ee4c5def9e45620db601325cc578f19a4042
PrzEllam <PerezAlex088@outlook.com>
2022-06-26 00:39:58 -0500

    ImGui Docking

---
+++ Photoxel/src/Builds/ImGuiBuild.cpp
@@ -0,0 +1,7 @@
+#include "fxpch.h"
+
+#include <imgui.cpp>
+#include <imgui_demo.cpp>
+#include <imgui_draw.cpp>
+#include <imgui_tables.cpp>
+#include <imgui_widgets.cpp>
---
+++ Photoxel/src/Builds/ImGuiImplBuild.cpp
@@ -0,0 +1,4 @@
+#include "fxpch.h"
+
+#include <backends/imgui_impl_dx11.cpp>
+#include <backends/imgui_impl_opengl3.cpp>
